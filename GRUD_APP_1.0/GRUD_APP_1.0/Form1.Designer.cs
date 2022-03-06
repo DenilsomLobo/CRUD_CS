a8e58d8bf753c2b62d67f85fb3cc19cf0a1eb200
Denilsom Lobo <denilsomlobo@gmail.com>
2022-03-05 22:49:44 -0300

    05/03/2022

---
+++ GRUD_APP_1.0/GRUD_APP_1.0/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace GRUD_APP_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.labelSexo = new System.Windows.Forms.Label();
+            this.textSexo = new System.Windows.Forms.TextBox();
+            this.labelData = new System.Windows.Forms.Label();
+            this.textData = new System.Windows.Forms.TextBox();
+            this.labelCidade = new System.Windows.Forms.Label();
+            this.textCidade = new System.Windows.Forms.TextBox();
+            this.labelCodcontato = new System.Windows.Forms.Label();
+            this.textCodcontato = new System.Windows.Forms.TextBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.labelConfirmar = new System.Windows.Forms.Label();
+            this.listViewBusca = new System.Windows.Forms.ListView();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 19);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(53, 20);
+            this.labelNome.TabIndex = 0;
+            this.labelNome.Text = "Nome:";
+            this.labelNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(71, 12);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(344, 27);
+            this.textNome.TabIndex = 1;
+            this.textNome.TextChanged += new System.EventHandler(this.textNome_TextChanged);
+            // 
+            // labelSexo
+            // 
+            this.labelSexo.AutoSize = true;
+            this.labelSexo.Location = new System.Drawing.Point(12, 63);
+            this.labelSexo.Name = "labelSexo";
+            this.labelSexo.Size = new System.Drawing.Size(80, 20);
+            this.labelSexo.TabIndex = 2;
+            this.labelSexo.Text = "Sexo(M/F):";
+            this.labelSexo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textSexo
+            // 
+            this.textSexo.Location = new System.Drawing.Point(98, 56);
+            this.textSexo.Name = "textSexo";
+            this.textSexo.Size = new System.Drawing.Size(36, 27);
+            this.textSexo.TabIndex = 3;
+            this.textSexo.TextChanged += new System.EventHandler(this.textSexo_TextChanged);
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(12, 117);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(44, 20);
+            this.labelData.TabIndex = 4;
+            this.labelData.Text = "Data:";
+            // 
+            // textData
+            // 
+            this.textData.Location = new System.Drawing.Point(62, 110);
+            this.textData.Name = "textData";
+            this.textData.Size = new System.Drawing.Size(191, 27);
+            this.textData.TabIndex = 5;
+            // 
+            // labelCidade
+            // 
+            this.labelCidade.AutoSize = true;
+            this.labelCidade.Location = new System.Drawing.Point(12, 168);
+            this.labelCidade.Name = "labelCidade";
+            this.labelCidade.Size = new System.Drawing.Size(59, 20);
+            this.labelCidade.TabIndex = 6;
+            this.labelCidade.Text = "Cidade:";
+            // 
+            // textCidade
+            // 
+            this.textCidade.Location = new System.Drawing.Point(77, 161);
+            this.textCidade.Name = "textCidade";
+            this.textCidade.Size = new System.Drawing.Size(338, 27);
+            this.textCidade.TabIndex = 7;
+            // 
+            // labelCodcontato
+            // 
+            this.labelCodcontato.AutoSize = true;
+            this.labelCodcontato.Location = new System.Drawing.Point(12, 223);
+            this.labelCodcontato.Name = "labelCodcontato";
+            this.labelCodcontato.Size = new System.Drawing.Size(90, 20);
+            this.labelCodcontato.TabIndex = 8;
+            this.labelCodcontato.Text = "Codcontato:";
+            // 
+            // textCodcontato
+            // 
+            this.textCodcontato.Location = new System.Drawing.Point(108, 220);
+            this.textCodcontato.Name = "textCodcontato";
+            this.textCodcontato.Size = new System.Drawing.Size(307, 27);
+            this.textCodcontato.TabIndex = 9;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(12, 281);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(94, 29);
+            this.buttonSalvar.TabIndex = 10;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // labelConfirmar
+            // 
+            this.labelConfirmar.Location = new System.Drawing.Point(1, 429);
+            this.labelConfirmar.Name = "labelConfirmar";
+            this.labelConfirmar.Size = new System.Drawing.Size(218, 23);
+            this.labelConfirmar.TabIndex = 11;
+            this.labelConfirmar.Click += new System.EventHandler(this.labelConfirmar_Click);
+            // 
+            // listViewBusca
+            // 
+            this.listViewBusca.Location = new System.Drawing.Point(441, 45);
+            this.listViewBusca.Name = "listViewBusca";
+            this.listViewBusca.Size = new System.Drawing.Size(595, 388);
+            this.listViewBusca.TabIndex = 12;
+            this.listViewBusca.UseCompatibleStateImageBehavior = false;
+            this.listViewBusca.SelectedIndexChanged += new System.EventHandler(this.listViewBusca_SelectedIndexChanged);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(441, 10);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(94, 29);
+            this.buttonBuscar.TabIndex = 13;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 448);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.listViewBusca);
+            this.Controls.Add(this.labelConfirmar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.textCodcontato);
+            this.Controls.Add(this.labelCodcontato);
+            this.Controls.Add(this.textCidade);
+            this.Controls.Add(this.labelCidade);
+            this.Controls.Add(this.textData);
+            this.Controls.Add(this.labelData);
+            this.Controls.Add(this.textSexo);
+            this.Controls.Add(this.labelSexo);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.labelNome);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelNome;
+        private TextBox textNome;
+        private Label labelSexo;
+        private TextBox textSexo;
+        private Label labelData;
+        private TextBox textData;
+        private Label labelCidade;
+        private TextBox textCidade;
+        private Label labelCodcontato;
+        private TextBox textCodcontato;
+        private Button buttonSalvar;
+        private Label labelConfirmar;
+        private ListView listViewBusca;
+        private Button buttonBuscar;
+    }
+}
---
+++ GRUD_APP_1.0/GRUD_APP_1.0/Form1.cs
@@ -0,0 +1,139 @@
+using MySql.Data.MySqlClient;
+
+namespace GRUD_APP_1._0
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            listViewBusca.View = View.Details;
+            listViewBusca.LabelEdit = true;
+            listViewBusca.AllowColumnReorder = true;
+            listViewBusca.FullRowSelect = true;
+            listViewBusca.GridLines = true;
+
+            listViewBusca.Columns.Add("Id", 30, HorizontalAlignment.Left);
+            listViewBusca.Columns.Add("Nome", 100, HorizontalAlignment.Left);
+            listViewBusca.Columns.Add("Sexo", 30, HorizontalAlignment.Left);
+            listViewBusca.Columns.Add("Data", 100, HorizontalAlignment.Left);
+            listViewBusca.Columns.Add("Cidade", 100, HorizontalAlignment.Left);
+            listViewBusca.Columns.Add("Codcontato", 100, HorizontalAlignment.Left);
+
+        }
+
+        private void textNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textSexo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelConfirmar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Iniciando a conexão com o MySql
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+                    cnn.ConnectionString = "datasource=localhost;username=root;password= ;database= db_cadastro";
+                    cnn.Open();
+                    // Variavel  para adcionar os textos da caixa de texto apos clicar no botão salvar.
+                    string sql = "INSERT INTO `db_cadastro`.`contato` (`nome`, `sexo`, `data`, `cidade`, `codcontato`)  VALUES ('"+textNome.Text+"', '"+textSexo.Text+"', '"+textData.Text+"', '"+textCidade.Text+"', '"+textCodcontato.Text+"')";
+                    MySqlCommand comando = new MySqlCommand(sql, cnn);
+                    // Labal de verificação com a conexão
+                    labelConfirmar.Text = "Conexão com MySQL com sucesso!";
+                    comando.ExecuteReader();
+
+                    MessageBox.Show("Contato salvo com sucesso !");
+                    cnn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // iniciando conexão com o MySql
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+
+                    cnn.ConnectionString = "datasource=localhost;username=root;password= ;database= db_cadastro";
+                    cnn.Open();
+                    // Variavel de busca:
+                    string busca = "SELECT * FROM `contato` ORDER by `cidade`, `sexo` DESC";
+                    MySqlCommand comando = new MySqlCommand(busca, cnn);
+                    labelConfirmar.Text = "Conexão com MySQL com sucesso!";
+                    comando.ExecuteReader();
+
+                    MessageBox.Show("Busca feita com sucesso !");
+                    using MySqlDataReader reader = comando.ExecuteReader();
+
+                    listViewBusca.Items.Clear();
+
+                    while (reader.Read()) 
+                    {
+                        string[] row = 
+                            {
+                            reader.GetString(0),
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetString(3),
+                            reader.GetString(4),
+                            reader.GetString(5),
+
+                        };
+
+                        var linha_listview = new ListViewItem(row);
+
+                        listViewBusca.Items.Add(linha_listview);
+
+                    }
+                    
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void listViewBusca_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
